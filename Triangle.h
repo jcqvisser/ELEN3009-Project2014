338520d285df11bdbf6f0a0f22d792c547940d6e
jcqvisser <jcqvisser@gmail.com>
2014-10-05 15:28:29 +0200

    Started Triangle Comments

--- Triangle.h
+++ Triangle.h
@@ -15,11 +15,23 @@ using namespace std;
 class argument_exceeds_number_of_coordinates{};
 class provide_positive_argument{};
 
+/*! \class Triangle
+*	\brief The triangle class represents a triangle in two-dimensional space. It is defined by a vector of three coordinates.
+*	
+*	The class is mainly used to determine whether a coordinate is within the defined triangular area.
+*/
 class Triangle {
 public:
+/*! Constructor
+*	\param coord0, coord1 and coord2 define the three corners of the triangle.
+*
+*/
 	Triangle(const shared_ptr<Coordinate>& coord0,
 			 const shared_ptr<Coordinate>& coord1,
 			 const shared_ptr<Coordinate>& coord2);
+/*!
+*	Default Virtual Destructor
+*/
 	virtual ~Triangle();
 	bool hasInside(const shared_ptr<Coordinate>& coord) const;
 	bool hasInside(const vector<shared_ptr<Coordinate>>& coords) const;
