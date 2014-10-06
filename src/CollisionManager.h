a1b68da19f926ac408f00740d6ab1377f4d1417f
jcqvisser <jcqvisser@gmail.com>
2014-10-06 19:00:28 +0200

    bleh

--- src/CollisionManager.h
+++ src/CollisionManager.h
@@ -9,14 +9,31 @@
 
 class CollisionManager {
 public:
-	CollisionManager(){};
-	virtual ~CollisionManager();
+	CollisionManager(){}; /**< Default Constructor*/
+	virtual ~CollisionManager(); /**< Standard Virtual Destructor*/
+
+	/*!
+	*	/brief Sets the vector of shared pointers to GameObjects to be iterated through to find collisions.
+	*/
 	void setGameObjecs(const vector<shared_ptr<GameObject>>& gameObjects);
+
+	/*!
+	*	\Brief Finds collsions between listed GameObjects
+	*
+	*	Enumerates all the non-ordered combinations of two GameObjects from _gameObjects and tests whether they collide by attempting to create a Collision object using the two objects and catching any exceptions thrown when these two GameObject objects do not collide.
+	*/
 	void findCollisions();
+
+	/*!
+	*	/brief Resolves all listed Collision objects.
+	*/
 	void ResolveCollisions(const float stepTime);
-	int numCollisions();
-	void purgeCollisions();
-	void purgeObjects();
+
+	int numCollisions(); /**< Returns the number of collisions */
+
+	void purgeCollisions(); /**< Removes all Collision objects- must be done when they are all resolved. */
+
+	void purgeObjects(); /**< Removes all GameObjects*/
 
 private:
 	vector<shared_ptr<GameObject>> _gameObjects;
