b78ea84342acb74b84cce761adecf529173d6f0f
jcqvisser <jcqvisser@gmail.com>
2014-10-06 17:14:43 +0200

    Added Doxygen comments for Boundary.h

--- src/Boundary.h
+++ src/Boundary.h
@@ -9,21 +9,35 @@
 #define BOUNDARY_H_
 #include "CollisionManager.h"
 
+/** Direction Enumeration. Each value represents a side of the window.*/
 enum direction{
-	NORTH,
-	SOUTH,
-	EAST,
-	WEST
+	NORTH, /**<Represents the top side*/
+	SOUTH, /**<Represents the bottom side*/
+	EAST, /**<Represents the right side*/
+	WEST /**<Represents the left side*/
 };
 
-
+/*!
+*	\brief Represents a boundary to the window.
+*
+*	GameObjects are able to move off the window, unless their path is blocked by some other GameObject.
+*	Boundary objects fulfill this purpose. They are created using the window dimentions and an enum value, thus the four boundaries can be created easily. Furthermore, they are immovable, so once the four Boundary objects are created all the GameObjects created in the window are constrained to the window, assuming working collision management.
+*/
 class Boundary : public GameObject
 {
 public:
+	/*!
+	*	\brief Creates on of the four screen boundaries.
+	*	\param hres Horisontal window resolution
+	*	\param vres Vertical window resolution
+	*	\param direction An enum indicating which of the four borders to create.
+	*/
 	Boundary(const int hres, const int vres, const direction);
-	virtual ~Boundary();
 
-	virtual void react(const Coordinate&) {};
+	/*
+	*	\brief Standard virtual destructor.
+	*/
+	virtual ~Boundary();
 };
 
 #endif /* BOUNDARY_H_ */
